3bc3f7462ac6d1cbd3541f74480476dbef93b217
gaocy6 <gaocy6@asiainfo-sec.com>
2020-05-07 20:46:47 +0800

    test

---
+++ src/main/java/com/example/gao/controller/HelloworldController.java
@@ -0,0 +1,12 @@
+package com.example.gao.controller;
+
+/**
+ * @author gaocy
+ * @create 2020-05-07 20:43
+ */
+public class HelloworldController {
+
+    public void show(){
+        System.out.println("helloworld !");
+    }
+}
